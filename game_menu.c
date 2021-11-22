f8ccbb3717e24facd283863c90654c5981ec542e
MSH <84580747+manitshegde@users.noreply.github.com>
2021-11-22 20:16:16 +0530

    update

--- game_menu.c
+++ game_menu.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int i,j;char tictactoe[5][5];
+int i,j,return_check;char tictactoe[5][5];
 //functions
 //tic tac toe function
 void print_updated_grid_ttt(){
@@ -13,6 +13,60 @@ void print_updated_grid_ttt(){
                         }               
                 printf("\n");
                 }
+}
+int input_compare_ttt_X(){//to check 'X';return1;
+                    if((tictactoe[0][0]=='X') && (tictactoe[0][2]=='X') && (tictactoe[0][4]=='X')){
+                        printf("'X' won the match\n");
+                        return_check=1;}//1
+                    else if((tictactoe[1][0]=='X') && (tictactoe[1][2]=='X') && (tictactoe[1][4]=='X')){
+                        printf("'X' won the match\n");
+                        return_check=1;}//2
+                    else if((tictactoe[2][0]=='X') && (tictactoe[2][2]=='X') && (tictactoe[2][4]=='X')){
+                        printf("'X' won the match\n");
+                        return_check=1;}//3
+                    else if((tictactoe[0][0]=='X') && (tictactoe[1][0]=='X') && (tictactoe[2][0]=='X')){
+                        printf("'X' won the match\n");
+                        return_check=1;}//4
+                    else if((tictactoe[0][2]=='X') && (tictactoe[1][2]=='X') && (tictactoe[2][2]=='X')){
+                        printf("'X' won the match\n");
+                        return_check=1;}//5
+                    else if((tictactoe[0][4]=='X') && (tictactoe[1][4]=='X') && (tictactoe[2][4]=='X')){
+                        printf("'X' won the match\n");
+                        return_check=1;}//6
+                    else if((tictactoe[0][0]=='X') && (tictactoe[1][2]=='X') && (tictactoe[2][4]=='X')){
+                        printf("'X' won the match\n");
+                        return_check=1;}//7
+                    else if((tictactoe[0][4]=='X') && (tictactoe[1][2]=='X') && (tictactoe[2][0]=='X')){
+                        printf("'X' won the match\n");
+                        return_check=1;}//8
+                    return return_check;
+}
+int input_compare_ttt_O(){//to check 'O';return2;
+                    if((tictactoe[0][0]=='O') && (tictactoe[0][2]=='O') && (tictactoe[0][4]=='O')){
+                        printf("'O' won the match\n");
+                        return_check=2;}//1
+                    else if((tictactoe[1][0]=='O') && (tictactoe[1][2]=='O') && (tictactoe[1][4]=='O')){
+                        printf("'O' won the match\n");
+                        return_check=2;}//2
+                    else if((tictactoe[2][0]=='O') && (tictactoe[2][2]=='O') && (tictactoe[2][4]=='O')){
+                        printf("'O' won the match\n");
+                        return_check=2;}//3
+                    else if((tictactoe[0][0]=='O') && (tictactoe[1][0]=='O') && (tictactoe[2][0]=='O')){
+                        printf("'O' won the match\n");
+                        return_check=2;}//4
+                    else if((tictactoe[0][2]=='O') && (tictactoe[1][2]=='O') && (tictactoe[2][2]=='O')){
+                        printf("'O' won the match\n");
+                        return_check=2;}//5
+                    else if((tictactoe[0][4]=='O') && (tictactoe[1][4]=='O') && (tictactoe[2][4]=='O')){
+                        printf("'O' won the match\n");
+                        return_check=2;}//6
+                    else if((tictactoe[0][0]=='O') && (tictactoe[1][2]=='O') && (tictactoe[2][4]=='O')){
+                        printf("'O' won the match\n");
+                        return_check=2;}//7
+                    else if((tictactoe[0][4]=='O') && (tictactoe[1][2]=='O') && (tictactoe[2][0]=='O')){
+                        printf("'O' won the match\n");
+                        return_check=2;}//8
+                    return return_check;
 }
 int games(int start_selection){
     //declaration for game 2
@@ -164,10 +218,10 @@ int games(int start_selection){
                     break;
             case 3:game3: printf("TIC TAC TOE GAME\n");
             //game code
-            char assign_player1,assign_player2;
+            char assign_player1,assign_player2;int playagain_ttt;
             printf("player 1 to choose sides...to choose 'O' , press 'O' .. OR to choose 'X' , press 'X'... :)\n");
             scanf("%s",&player_selection_game3);
-            if(player_selection_game3=='o' || player_selection_game3=='X'){
+            if(player_selection_game3=='o' || player_selection_game3=='O'){
                 assign_player1='O';
                 assign_player2='X';
                 printf("player 1 will play with 'O' and player 2 will play with 'X'\n");
@@ -179,7 +233,7 @@ int games(int start_selection){
             }
             else{
                 printf("invalid input..\n");
-                //goto game3;
+                goto game3;
             }
             for ( i = 0; i < 3; i++)
                 {
@@ -197,10 +251,12 @@ int games(int start_selection){
                 printf("\n");
                 }
                 //accept user input
+                if(assign_player2=='O'){
+                    goto player2_input_ttt;
+                }
                 int player1_position_ttt,player2_position_ttt;
-                //you need to block the reserved position i.e the grid
                 while(1){
-                    player1_input_ttt:printf("player 1 : enter the position (1-9)\n");
+                    player1_input_ttt:printf("player 1 : enter the position (1-9)\t");
                     scanf("%d",&player1_position_ttt);
                     switch(player1_position_ttt){
                         case 1: tictactoe[0][0]=assign_player1;
@@ -234,7 +290,41 @@ int games(int start_selection){
                                  goto player1_input_ttt;
                                  break;
                     }
-                    player2_input_ttt:printf("player 2: enter the position(1-9)\n");
+                        input_compare_ttt_X();
+                        if(return_check==1){
+                        printf("to play again press '1' or to go back press '0'\n");
+                        scanf("%d",&playagain_ttt);
+                        if(playagain_ttt==1){
+                            printf("play again..\n");
+                            goto game3;
+                        }
+                        else if(playagain_ttt==0){
+                            printf("going back to game menu\n");
+                            return 1;//main function
+                        }
+                        else{
+                            printf("default:play again\n");
+                            goto game3;
+                        }
+                    }
+                    input_compare_ttt_O();
+                    if(return_check==2){
+                        printf("to play again press '1' or to go back press '0'\n");
+                        scanf("%d",&playagain_ttt);
+                        if(playagain_ttt==1){
+                            printf("play again..\n");
+                            goto game3;
+                        }
+                        else if(playagain_ttt==0){
+                            printf("going back to game menu\n");
+                            return 1;//main function
+                        }
+                        else{
+                            printf("default:play again\n");
+                            goto game3;
+                        }
+                    }
+                    player2_input_ttt:printf("player 2: enter the position(1-9)\t");
                     scanf("%d",&player2_position_ttt);
                     switch(player2_position_ttt){
                         case 1: tictactoe[0][0]=assign_player2;
@@ -268,9 +358,42 @@ int games(int start_selection){
                                  goto player2_input_ttt;
                                  break;
                     }
-
-                }//input while ttt
-
+                    input_compare_ttt_O();
+                    if(return_check==2){
+                        printf("to play again press '1' or to go back press '0'\n");
+                        scanf("%d",&playagain_ttt);
+                        if(playagain_ttt==1){
+                            printf("play again..\n");
+                            goto game3;
+                        }
+                        else if(playagain_ttt==0){
+                            printf("going back to game menu\n");
+                            return 1;//main function
+                        }
+                        else{
+                            printf("default:play again\n");
+                            goto game3;
+                        }
+                    }
+                    input_compare_ttt_X();
+                    if(return_check==1){
+                        printf("to play again press '1' or to go back press '0'\n");
+                        scanf("%d",&playagain_ttt);
+                        if(playagain_ttt==1){
+                            printf("play again..\n");
+                            goto game3;
+                        }
+                        else if(playagain_ttt==0){
+                            printf("going back to game menu\n");
+                            return 1;//main function
+                        }
+                        else{
+                            printf("default:play again\n");
+                            goto game3;
+                        }
+                    }
+                }//while-input
+                //testing for inputs
             break;
             //next case
             }//switch_block
